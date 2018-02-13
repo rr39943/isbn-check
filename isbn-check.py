be324f9579eb9df68c4e111aabf640cf2a0f6a06
Rey Raphaël <raphael.rey@epfl.ch>
2018-02-13 11:21:02 +0100

    transform to python 3

--- isbn-check.py
+++ isbn-check.py
@@ -11,7 +11,7 @@ def isbn_check(number):
         if i.isdigit():
             pass
         else:
-            print "Incorrect ISBN. (Letters instead of numbers)"  # or return False
+            print("Incorrect ISBN. (Letters instead of numbers)")  # or return False
             return False
 
     # check if 10 or 13
@@ -20,20 +20,21 @@ def isbn_check(number):
                     int(isbn[3]) * 7 + int(isbn[4]) * 6 + int(isbn[5]) * 5 + int(isbn[6]) * 4 + \
                     int(isbn[7]) * 3 + int(isbn[8]) * 2 + int(isbn[9]) * 1
         if sum_check % 11 == 0:
-            print "Correct ISBN."  # or return True
+            print ("Correct ISBN.")  # or return True
         else:
-            print "Incorrect ISBN."  # or return False
+            print ("Incorrect ISBN.")  # or return False
     elif len(isbn) == 13:
         sum_check = int(isbn[0]) * 1 + int(isbn[1]) * 3 + int(isbn[2]) * 1 + int(isbn[3]) * 3 + int(
             isbn[4]) * 1 + int(isbn[5]) * 3 + int(isbn[6]) * 1 + int(isbn[7]) * 3 + int(
             isbn[8]) * 1 + int(isbn[9]) * 3 + int(isbn[10]) * 1 + int(isbn[11]) * 3 + int(
             isbn[12]) * 1
         if sum_check % 10 == 0:
-            print "Correct ISBN."  # or return True
+            print("Correct ISBN.")  # or return True
         else:
-            print "Incorrect ISBN."  # or return False
+            print ("Incorrect ISBN.")  # or return False
     else:
-        print "Incorrect ISBN. (Wrong length)"
+        print ("Incorrect ISBN. (Wrong length)")
+
 
 
 # DEBUG tests
